dffa0750a98920942c3e397fa9b0264d70383512
Javi Jiménez <javi.jimenez@guifi.net>
2013-10-31 14:44:33 +0100

    Debian installer graphical environment too.

--- live-build.sh
+++ live-build.sh
@@ -31,6 +31,12 @@ do
           --architectures i386 \
 	  --mode debian
 #  --volatile false \ # in unstable doesn't exists that option
+# Source: http://live.debian.net/manual/3.x/html/live-manual.en.html
+# If you want a desktop debian installer do:
+#$ lb config --architectures i386 --linux-flavours 486 \
+#         --debian-installer live
+#$ echo debian-installer-launcher >> config/package-lists/my.list.chroot
+      echo debian-installer-launcher >> $BASE_DIR/build/$distro/live-build-$distro-$machine/config/package-lists/di-launcher.chroot
       cd -
       echo "Copying requested '$machine' packages to the build dir."
       for package in `cat $BASE_DIR/conf/$distro/$machine.packages | sort -u`
